596447c70c95ba7551d6d158cf86cae6ede6c770
abi-sae <125155934+abi-sae@users.noreply.github.com>
2023-06-22 13:04:57 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+'''Importamos todos los modulos para que el programa principal pueda funcionar.
+Se despliega el menu para que se muestren las opciones que se pueden seleccionar.'''
+
+import eliminar
+import mostrar
+import menu
+import buscar
+import agregar
+import file
+
+
+def menu_principal():
+    print("Menú principal")
+    print("1. Agregar un producto")
+    print("2. Buscar producto")
+    print("3. Eliminar un producto")
+    print("4. Mostrar el catálogo")
+    print("5. Cargar catálogo")
+    print("6. Guardar catálogo")
+    print("7. Salir")
+
+    opcion = input("Porfavor haga una seleccion: ")
+    return opcion
+
+def ejecutar_opcion(opcion):
+    if opcion == "1":
+        agregar.menu_agregar()
+    elif opcion == "2":
+        buscar.buscar_producto()
+    elif opcion == "3":
+        eliminar.eliminar_producto()
+    elif opcion == "4":
+        mostrar.mostrar_menu()
+    elif opcion == "5":
+        file.cargar_catalogo()
+    elif opcion == "6":
+        file.guardar_catalogo()
+    elif opcion == "7":
+        print("Adios")
+    else:
+        print("Seleccione una opcion valida.")
+
+def main():
+    while True:
+        opcion = menu_principal()
+        if opcion == "7":
+            break
+        ejecutar_opcion(opcion)
+
+if __name__ == "__main__":
+    main()
